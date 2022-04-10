43e7d5b14fb162937c22bc18de28ac801c2b1ed4
nakai-ryoichi <nakai-ryoichi@dmm.com>
2022-04-10 13:55:04 +0000

    refactor

--- go.mod
+++ go.mod
@@ -3,7 +3,5 @@ module TypingGame
 go 1.16
 
 require (
-	github.com/onsi/ginkgo v1.15.0 // indirect
-	github.com/onsi/gomega v1.10.5 // indirect
 	github.com/tjarratt/babble v0.0.0-20191209142150-eecdf8c2339d
 )
--- main.go
+++ main.go
@@ -2,77 +2,54 @@ package main
 
 import (
 	"bufio"
-	"context"
-	"flag"
 	"fmt"
-	"github.com/tjarratt/babble"
 	"io"
 	"os"
+	"strconv"
 	"time"
+  "math/rand"
 )
 
-// タイムリミットのデフォルト10秒
-const timeLimit = 10
-
-// スコア初期値
-var score = 0
-
-var (
-	babbler  babble.Babbler
-	question string
-	d        time.Duration
-)
-
-func init() {
-	flag.DurationVar(&d, "d", timeLimit*time.Second, "duration flag")
-	flag.Parse()
-
-	babbler = babble.NewBabbler()
-	babbler.Count = 1
+// TypingGameを扱う情報
+type GameInfo struct {
+	timeLimit time.Duration
+	score 		int
+	name			string
 }
 
 func main() {
-	startGame()
-}
+	// 初期化
+ 	g := GameInfo{time.Duration(10 * time.Second), 0, "defalut",}
+	stdin := os.Stdin
 
-// こうすることで_main関数が終わると同時のキャンセル処理が走る
-func startGame() {
-	// ゲーム開始前のカウントダウン表示
-	countdown()
-
-	// コンテキストのキャンセル処理
-	// ゴルーチンをまたいだ処理のキャンセルに使う
-	// done := time.After(timeLimit * time.Second)
-	bc := context.Background()
-	ctx, cancel := context.WithTimeout(bc, d)
-	defer cancel()
-
-	start := time.Now()
-	questions()
-
-	ch := input(os.Stdin)
-
-	for {
-		select {
-		case <-ctx.Done():
-			fmt.Println("\n\nTime UP!!")
-			fmt.Println("score:", score)
-			return // この行がないと無限ループになる
-		case v := <-ch:
-			if v == question {
-				score++
-				fmt.Println("Good!")
-			} else {
-				fmt.Println("oops...")
-			}
-			end := time.Now()
-			fmt.Printf("%d秒経過\n", int((end.Sub(start)).Seconds()))
-			questions()
-		}
+	// 名前入力
+	fmt.Print("名前を入力してね！(何も入力しない場合は defalut になります)\n > ")
+	name := infoInput(stdin)
+	if name != "" {
+		g.name = name
 	}
+	fmt.Printf("%s さんようこそ！\n", g.name)
 
+	// タイムリミットの設定
+	fmt.Print("タイムリミットを設定してね！(正しい値が入力されない場合は 10s になります) \n > ")
+	timeLimit := infoInput(stdin)
+	if v, err := strconv.Atoi(timeLimit); err == nil && v > 0 {
+		g.timeLimit = time.Duration(time.Duration(v) * time.Second)
+	}
+	fmt.Printf("タイムリミットは %v です！\n", g.timeLimit)
+
+	// カウントダウン開始
+	countdown()
+	
+	// ゲーム開始
+	g.score = game(g.timeLimit)
+
+	// 結果発表
+	time.Sleep(time.Second * 2)
+	fmt.Printf("\n %s さんのスコアは。。%d点でした！お疲れ様でした！ \n", g.name, g.score)
 }
 
+// 3秒間カウントダウンを行うコード
 func countdown() {
 	for i := 3; i > 0; i-- {
 		fmt.Printf("%d ", i)
@@ -81,21 +58,67 @@ func countdown() {
 	fmt.Println("Go!")
 }
 
-func questions() {
-	question = babbler.Babble()
-	fmt.Println("\ntype this:", question)
-	fmt.Print("> ")
+func game(t time.Duration) int {
+	words, err := questions()
+	if err != nil {
+		fmt.Println("エラーが起きたため、ゲームを終了します。")
+		os.Exit(1)
+	}
+
+	ch := gameInput(os.Stdin)
+	score := 0
+	timer := time.NewTimer(t)
+	defer timer.Stop()
+	LOOP:
+	for {
+		word := words[rand.Intn(len(words))]
+		fmt.Printf("入力する単語：%s \n > ", word)
+		select {
+		case res := <-ch:
+			if res == word {
+				score++
+				fmt.Println("+1点！！")
+			} else {
+				fmt.Println("不正解！！")
+			}
+		case <- timer.C:
+			fmt.Println("\n 終了！！！")
+			break LOOP
+		}
+	}
+	return score
 }
 
-func input(r io.Reader) <-chan string {
+// 自分で考えたお題を出すための関数
+func questions() ([]string, error) {
+	var words []string
+	f, err := os.Open("question.txt")
+	if err != nil {
+		return nil, fmt.Errorf("%v", err)
+	}
+	s := bufio.NewScanner(f)
+	for s.Scan() {
+		words = append(words, s.Text())
+	}
+	return words, nil
+}
+
+// 初期情報を入力するための関数
+func infoInput(r io.Reader) string {
+	s := bufio.NewScanner(r)
+	s.Scan()
+	return s.Text()
+}
+
+// タイピングを入力するための関数
+func gameInput(r io.Reader) <-chan string {
 	ch := make(chan string)
 	go func() {
-		// 標準入力から１行ずつ文字をよみこむ
 		s := bufio.NewScanner(r)
+		defer close(ch) // ここでチャネル閉じても意味なくないか？
 		for s.Scan() {
 			ch <- s.Text()
 		}
-		close(ch)
 	}()
 	return ch
 }
